1ffd1aeca86515ed281e2b08391f3c6892f11d72
emily <emily.a.h@hotmail.com>
2021-01-29 02:21:12 -0500

    test- added api.js routes to server.js

--- server.js
+++ server.js
@@ -27,7 +27,39 @@ app.get("/", (req, res) => {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({body}, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
